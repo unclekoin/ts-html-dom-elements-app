e787a35f7fb871df4e63d4450297662bbd369375
Pavel Koryakin <unclekoin@gmail.com>
2022-06-23 21:49:06 +0300

    feat: change layout and add styles

--- src/app.ts
+++ src/app.ts
@@ -200,7 +200,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listId = `${this.type}-project-list`;
     this.element.querySelector("ul")!.id = listId;
     this.element.querySelector("h2")!.textContent =
-      this.type.toUpperCase() + " PROJECT";
+      this.type.toUpperCase()[0] + this.type.slice(1) + " Project";
   }
 
   private renderProjects() {
